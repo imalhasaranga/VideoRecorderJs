25e5008688bd929075495a9c70e452dc87c3d504
imalhasaranga <imaa95@gmail.com>
2014-05-17 15:17:03 +0530

    Increased flexibility and reducing overall file size

--- fileupload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-move_uploaded_file($_FILES["video_data"]["tmp_name"],"video/Testaudio.webm");
-move_uploaded_file($_FILES["audio_data"]["tmp_name"],"video/Testaudio.wav");
-
-echo "Done : ";
-
-?>
